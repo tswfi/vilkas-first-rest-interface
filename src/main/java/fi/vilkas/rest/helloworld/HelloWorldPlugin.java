845a19fee2a4d600a99685a2cd785db90609b84b
Tatu Wikman (training2 laptop) <root@training-2.lan>
2014-03-25 16:18:47 +0100

    decoupled to interface and implementation

--- src/main/java/fi/vilkas/rest/helloworld/HelloWorldPlugin.java
+++ src/main/java/fi/vilkas/rest/helloworld/HelloWorldPlugin.java
@@ -1,5 +1,7 @@
 package fi.vilkas.rest.helloworld;
 
+import javax.inject.Singleton;
+
 import com.epages.plugin.AbstractPlugin;
 
 public class HelloWorldPlugin extends AbstractPlugin {
@@ -8,7 +10,8 @@ public class HelloWorldPlugin extends AbstractPlugin {
 	protected void configure() {
 		// bind to rest JaxRs stuff
 		bindToJaxRs(HelloWorldResource.class);
-
+//		bind(HelloWorldService.class).to(HelloWorldServiceImpl.class).in(Singleton.class);
+		bind(HelloWorldService.class).to(HelloWorldServiceImpl2.class).in(Singleton.class);
 	}
 
 }
--- src/main/java/fi/vilkas/rest/helloworld/HelloWorldResource.java
+++ src/main/java/fi/vilkas/rest/helloworld/HelloWorldResource.java
@@ -1,5 +1,6 @@
 package fi.vilkas.rest.helloworld;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -7,9 +8,16 @@ import javax.ws.rs.core.MediaType;
 
 @Path("v/hello-world")
 public class HelloWorldResource {
+	private final HelloWorldService service;
+
+	@Inject
+	public HelloWorldResource(HelloWorldService service) {
+		this.service = service;
+	}
+	
 	@GET
 	@Produces(( MediaType.TEXT_PLAIN ))
 	public String getHelloWorld() {
-		return "Hello World!";
+		return service.sayHelloWorld();
 	}
 }
